b58555806bf34a890f15ca365330c823d1b02d42
IvanD1061 <ivan.duenas@wsu.edu>
2025-03-12 12:46:41 -0700

    Added setBursts, first sets the CPU bursts and the IOBurst

--- scheduler.c
+++ scheduler.c
@@ -10,3 +10,19 @@ SchedulerStats initStats()
     stats.TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0;
     return stats;
 }
+
+static void setBursts(Process *p)
+{
+    FILE *random_num_file = fopen("random-numbers", "r");
+    if (random_num_file == NULL)
+    {
+        printf("Error: Cannot open file random-numbers\n");
+        exit(1);
+    }
+    // Set the CPU burst time
+    p->CPUBurst = randomOS(p->B, p->processID, random_num_file);
+    // Set the IO burst time
+    p->IOBurst = p->M * p->CPUBurst;
+
+    fclose(random_num_file);
+}
