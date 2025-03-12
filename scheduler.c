0f3547d9092da0cda9db215a1024bbf8b9cf10b7
IvanD1061 <ivan.duenas@wsu.edu>
2025-03-12 11:04:33 -0700

    removed random from scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -42,49 +42,6 @@ uint32_t TOTAL_FINISHED_PROCESSES = 0;  // The total number of processes that ha
 uint32_t TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0; // The total cycles in the blocked state
 
 const char* RANDOM_NUMBER_FILE_NAME= "random-numbers";
-const uint32_t SEED_VALUE = 200;  // Seed value for reading from file
-
-// Additional variables as needed
-
-
-/**
- * Reads a random non-negative integer X from a file with a given line named random-numbers (in the current directory)
- */
-uint32_t getRandNumFromFile(uint32_t line, FILE* random_num_file_ptr){
-    uint32_t end, loop;
-    char str[512];
-
-    rewind(random_num_file_ptr); // reset to be beginning
-    for(end = loop = 0;loop<line;++loop){
-        if(0==fgets(str, sizeof(str), random_num_file_ptr)){ //include '\n'
-            end = 1;  //can't input (EOF)
-            break;
-        }
-    }
-    if(!end) {
-        return (uint32_t) atoi(str);
-    }
-
-    // fail-safe return
-    return (uint32_t) 1804289383;
-}
-
-
-
-/**
- * Reads a random non-negative integer X from a file named random-numbers.
- * Returns the CPU Burst: : 1 + (random-number-from-file % upper_bound)
- */
-uint32_t randomOS(uint32_t upper_bound, uint32_t process_indx, FILE* random_num_file_ptr)
-{
-    char str[20];
-    
-    uint32_t unsigned_rand_int = (uint32_t) getRandNumFromFile(SEED_VALUE+process_indx, random_num_file_ptr);
-    uint32_t returnValue = 1 + (unsigned_rand_int % upper_bound);
-
-    return returnValue;
-} 
-
 
 /********************* SOME PRINTING HELPERS *********************/
 
