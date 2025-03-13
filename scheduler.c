787db5023679b49137db233b019167897010be0c
IvanD1061 <ivan.duenas@wsu.edu>
2025-03-13 12:17:57 -0700

    Added RR Working

--- scheduler.c
+++ scheduler.c
@@ -226,3 +226,113 @@ void sjf(Process *p_list, SchedulerStats *stats)
     printProcessSpecifics(p_list, *stats);
     printSummaryData(p_list, *stats);
 }
+void rr(Process *p_list, SchedulerStats *stats)
+{
+    printf("**** \n\n\n\n Round Robin\n");
+
+    Process *rp = NULL; // Running process
+    Process *p = NULL;  // Process
+
+    while (stats->TOTAL_FINISHED_PROCESSES < stats->TOTAL_CREATED_PROCESSES)
+    {
+
+        for (uint32_t i = 0; i < stats->TOTAL_CREATED_PROCESSES; i++)
+        {
+            uint32_t index = i;
+            if (rp != NULL)
+            {
+                index = (rp - p_list) + i;
+            }
+
+            if (index >= stats->TOTAL_CREATED_PROCESSES) // Wrap the index if it goes beyond the number of processes
+            {
+                index -= stats->TOTAL_CREATED_PROCESSES; // Adjust index to wrap around to the beginning
+            }
+
+            p = &p_list[index];
+            // Terminated
+            if (p->status == TERMINATED)
+            {
+                continue;
+            }
+
+            // Blocked
+            if (p->status == BLOCKED)
+            {
+                p->currentIOBlockedTime++;
+                p->IOBurst--;
+
+                // Blocked -> Ready
+                if (p->IOBurst <= 0)
+                {
+                    p->status = READY;
+                }
+
+                stats->TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED++;
+            }
+
+            // Not started -> Ready
+            else if (p->status == NOT_STARTED)
+            {
+
+                p->status = READY;
+                stats->TOTAL_STARTED_PROCESSES++;
+                p->isFirstTimeRunning = 1;
+            }
+
+            // Ready
+            if (p->status == READY)
+            {
+                // Ready -> Running
+                if (rp == NULL)
+                {
+                    setBursts(p);
+                    p->status = RUNNING;
+                    rp = p;
+                }
+                else
+                {
+                    p->currentWaitingTime++;
+                }
+            }
+
+            // Running
+            else if (p->status == RUNNING)
+            {
+                p->currentCPUTimeRun++;
+                p->CPUBurst--;
+                stats->TOTAL_STARTED_PROCESSES += p->isFirstTimeRunning;
+                p->isFirstTimeRunning = 0;
+
+                // Running -> Terminate
+                if (p->currentCPUTimeRun >= p->C)
+                {
+                    p->status = TERMINATED;
+                    p->finishingTime = stats->CURRENT_CYCLE;
+                    rp = NULL;
+
+                    stats->TOTAL_FINISHED_PROCESSES++;
+                }
+
+                // Running -> Block
+                else if (p->CPUBurst <= 0)
+                {
+                    rp = NULL;
+                    p->status = BLOCKED;
+                }
+                // Running -> Ready
+                else if (p->quantum > 0 && p->currentCPUTimeRun % p->quantum == 0)
+                {
+                    p->status = READY;
+                    rp = NULL;
+                }
+            }
+        }
+
+        stats->CURRENT_CYCLE++;
+    }
+    printStart(p_list, *stats);
+    printFinal(p_list, *stats);
+    printProcessSpecifics(p_list, *stats);
+    printSummaryData(p_list, *stats);
+}
